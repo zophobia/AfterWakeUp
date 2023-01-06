b0610ac616c4473511e52673d2873769a3834e07
zophobia <122071029+zophobia@users.noreply.github.com>
2023-01-06 14:36:16 +0900

    Add files via upload

---
+++ clock.js
@@ -0,0 +1,16 @@
+const clock = document.querySelector("h2#clock");
+
+function sayHello() {
+    console.log("hello");
+}
+
+function getClock() {
+    const date = new Date();
+    const hours = String(date.getHours()).padStart(2,"0");
+    const minutes = String(date.getMinutes()).padStart(2,"0");
+    const second = String(date.getSeconds()).padStart(2,"0");
+    clock.innerText = `${hours}:${minutes}:${second}`;
+}
+
+getClock();
+setInterval(getClock, 1000);
