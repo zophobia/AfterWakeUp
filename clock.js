91d425c9e1b55e3547ae83b31cad4b23787740b8
zophobia <122071029+zophobia@users.noreply.github.com>
2023-01-06 14:37:45 +0900

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,9 +1,5 @@
 const clock = document.querySelector("h2#clock");
 
-function sayHello() {
-    console.log("hello");
-}
-
 function getClock() {
     const date = new Date();
     const hours = String(date.getHours()).padStart(2,"0");
